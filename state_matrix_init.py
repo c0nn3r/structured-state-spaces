09d7f2e06c1aa9d38d69db2696b7e7ff3144d383
Conner Vercellino <connervercellino@gmail.com>
2022-08-15 14:24:42 -0700

    Add (for now blank) init funcs

---
+++ state_matrix_init.py
@@ -0,0 +1,14 @@
+import torch as th
+
+
+def init_state_space(size, name):
+    pass
+
+
+def warped_scaled_legendre(size):
+    pass
+
+
+def truncated_fourier(size):
+    # TODO: Add once correction is release
+    pass
