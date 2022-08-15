d76a3e29496ed4e8bb913831fb4eaec50f397b31
Conner Vercellino <connervercellino@gmail.com>
2022-08-15 14:54:07 -0700

    Add init for warped scaled legendre

--- state_matrix_init.py
+++ state_matrix_init.py
@@ -1,12 +1,26 @@
 import torch as th
 
 
+def numpy_like_meshgrid(size):
+    x = th.arange(size)
+    y = th.arange(size)
+    return th.meshgrid(x, y, indexing='ij')
+
+
 def init_state_space(size, name):
     pass
 
 
 def warped_scaled_legendre(size):
-    pass
+    n, k = numpy_like_meshgrid(size)
+
+    A = th.tril(th.ones((size, size)), -1)
+    A = th.where(n == k, (n + 1) / (2 * n + 1), A)
+    A *= -th.sqrt(2 * n + 1) * th.sqrt(2 * k + 1)
+
+    B = th.sqrt((2 * n[:,:1] + 1))
+
+    return A, B
 
 
 def truncated_fourier(size):
