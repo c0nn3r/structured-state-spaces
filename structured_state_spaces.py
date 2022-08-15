d593693d1613b218d89db791502ec855a39cbc43
Conner Vercellino <connervercellino@gmail.com>
2022-08-15 14:21:30 -0700

    Sketch of model

---
+++ structured_state_spaces.py
@@ -0,0 +1,29 @@
+import functorch
+import torch as th
+
+
+class SSMHead(th.nn.Module):
+
+    def __init__(self):
+        pass
+
+    def forward(self, x: th.Tensor):
+        pass
+
+
+class S4Block(th.nn.Module):
+
+    def __init__(self):
+        pass
+
+    def forward(self, x: th.Tensor):
+        pass
+
+
+class S4(th.nn.Module):
+
+    def __init__(self):
+        pass
+
+    def forward(self, x: th.Tensor):
+        pass
